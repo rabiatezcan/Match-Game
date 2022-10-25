116a25307cb0ac59dbb1f7804b3f1881e1cf2098
rabiatezcan <rabiatezcan@hotmail.com>
2022-10-25 10:56:15 +0300

    UI LevelView implemented

---
+++ Assets/Game/Scripts/UI/Screens/LevelView/LevelView.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelView : MonoBehaviour
+{
+    [SerializeField] private Text _levelTxt; 
+
+    public void Show()
+    {
+        SetLevelText();
+    }
+
+    private void SetLevelText()
+    {
+        _levelTxt.text = "Level " + PlayerHelper.Instance.Player.Level;
+    }
+}
