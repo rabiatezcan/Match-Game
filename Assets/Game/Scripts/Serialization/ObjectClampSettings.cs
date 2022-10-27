00da7dc039cecbf74f2914f2c495805ef94d87ec
rabiatezcan <rabiatezcan@hotmail.com>
2022-10-27 20:04:35 +0300

    rename

--- Assets/Game/Scripts/Serialization/ObjectClampSettings.cs
+++ Assets/Game/Scripts/Serialization/ObjectClampSettings.cs
@@ -2,18 +2,23 @@
 
 namespace GameCore
 {
-    [CreateAssetMenu(menuName = "Game/Movement Settings", order = 2)]
-    public class MovementSettings : ScriptableObject
+    [CreateAssetMenu(menuName = "Game/Object Clamp Settings", order = 2)]
+    public class ObjectClampSettings : ScriptableObject
     {
         [Header("Movement Parameters")]
         [SerializeField] private float _minX;
-        [SerializeField] private float _maxX; 
+        [SerializeField] private float _maxX;
         [SerializeField] private float _minY;
         [SerializeField] private float _maxY;
+        [SerializeField] private float _minZ;
+        [SerializeField] private float _maxZ;
+
 
         public float MinX => _minX;
         public float MaxX => _maxX;
         public float MinY => _minY;
         public float MaxY => _maxY;
+        public float MinZ => _minZ;
+        public float MaxZ => _maxZ;
     }
 }
