929377d95edb2c91be054a3973792737c796b142
rabiatezcan <rabiatezcan@hotmail.com>
2022-11-02 15:53:16 +0300

    coin animation fix

--- Assets/Game/Scripts/UI/Screens/CoinView/CoinImage.cs
+++ Assets/Game/Scripts/UI/Screens/CoinView/CoinImage.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,57 +9,38 @@ public class CoinImage : MonoBehaviour
     private Vector3 _middlePos;
     private Vector3 _targetPos;
     private Vector3 _defaultPos;
-    private float _time;
-    private bool _canMove;
-
-    [SerializeField] private List<GameObject> _objs;
     public void SetActive()
     {
-        gameObject.SetActive(true);
         _defaultPos = transform.localPosition;
+        gameObject.SetActive(true);
     }
     public void Hide()
     {
-        _canMove = false;
-        gameObject.SetActive(false);
         transform.localPosition = _defaultPos;
+        gameObject.SetActive(false);
     }
 
     public void SetMovePosition(Vector3 newPos)
     {
         SetActive();
 
-        _targetPos = newPos;
-        _time = 0f;
-        _canMove = true;
-        _startPos = transform.position;
-        _middlePos = _startPos + (Vector3.left * 2f);
-
-        _objs[0].transform.position = _startPos;
-        _objs[1].transform.position = _middlePos;
-        _objs[2].transform.position = _targetPos;
+        _targetPos = newPos + (Vector3.right * .25f);
+        _startPos = _defaultPos;
+        _middlePos = _startPos + (Vector3.left * 350f);
+        MoveToTargetPosition();
     }
 
     private void MoveToTargetPosition()
     {
-        //_startPos = transform.position;
-        //_middlePos = _startPos + (Vector3.left * 5f) + (Vector3.up * 3f) ;
-        //Vector3 startToMiddle = Vector3.Slerp(_startPos, _middlePos, Time.deltaTime);
-        //Vector3 middletoEnd = Vector3.Slerp(_middlePos, _targetPos, Time.deltaTime);
-        transform.position = Vector3.Slerp(transform.position, _targetPos, Time.deltaTime * 15f);
-
-
-        if (Vector3.Distance(transform.position, _targetPos) < .2f)
-        {
-            ScoreSystem.AddScore(1);
-            Hide();
-        }
-    }
-    private void Update()
-    {
-        if (_canMove)
-        {
-            MoveToTargetPosition();
-        }
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(transform.DOLocalMove(_middlePos, .25f))
+                .Insert(.2f, transform.DOMove(_targetPos, .45f))
+                .OnComplete
+                (() =>
+                    {
+                        ScoreSystem.AddScore(1);
+                        Hide();
+                    }
+                );
     }
 }
